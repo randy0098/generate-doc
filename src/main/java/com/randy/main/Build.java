de996973bd468350295e2c1896288fee369b30b0
Randy <randy.xia98@gmail.com>
2018-06-01 15:58:03 +0800

    代码优化。

--- src/main/java/com/randy/main/Build.java
+++ src/main/java/com/randy/main/Build.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.apache.commons.configuration2.ex.ConfigurationException;
@@ -20,7 +19,6 @@ import io.github.swagger2markup.Swagger2MarkupConfig;
 import io.github.swagger2markup.Swagger2MarkupConverter;
 import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
 import io.github.swagger2markup.markup.builder.MarkupLanguage;
-import io.swagger.models.Model;
 import io.swagger.models.Operation;
 import io.swagger.models.Path;
 import io.swagger.models.Swagger;
@@ -28,7 +26,8 @@ import io.swagger.models.Tag;
 import io.swagger.parser.SwaggerParser;
 
 public class Build {
-	private static String swaggerApi = "http://localhost:8091/v2/api-docs";
+	private final static String swaggerApi = "http://localhost:8091/v2/api-docs";
+	private final static String outputFileLocation = "target/generated-docs/asciidoc/all";
 	
 	public static void main(String[] args) throws ConfigurationException, IOException {
 		Properties properties = new Properties();
@@ -36,8 +35,8 @@ public class Build {
 	    properties.load(bufferedReader);
 	    String tags = properties.getProperty("tags");
 	    String[] array = tags.split(",");
-	    List<String> list = Arrays.asList(array);
-	    System.out.println(tags);
+	    List<String> filterlist = Arrays.asList(array);
+	    System.out.println(tags+":"+filterlist.size());
 		
 		Charset charset = Charset.defaultCharset();
 		System.out.println("charset:" + charset.name());
@@ -49,19 +48,26 @@ public class Build {
 			Tag tag = tagIte.next();
 			String name = tag.getName();
 			System.out.println("tag name:" + name);
-			if(name!=null && list.contains(name)) {
-				System.out.println("remain tag:" + name);
-			}else {
-				tagIte.remove();
-				System.out.println("remove tag:" + name);
+			
+			//tags配置不为空时才进行过滤操作
+			if(!tags.equalsIgnoreCase("")) {
+				if(name!=null && filterlist.contains(name)) {
+					System.out.println("remain tag:" + name);
+				}else {
+					//过滤不需要的tag
+					tagIte.remove();
+					System.out.println("remove tag:" + name);
+				}				
 			}
+
 		}
 		
-		Map<String,Model> definitions = swagger.getDefinitions();
+//		Map<String,Model> definitions = swagger.getDefinitions();
+		
 		Map<String,Path> paths = swagger.getPaths();
-		Iterator ite = paths.entrySet().iterator();
+		Iterator<Map.Entry<String, Path>> ite = paths.entrySet().iterator();
 		while(ite.hasNext()) {
-			Map.Entry entry = (Entry) ite.next();
+			Map.Entry<String, Path> entry = ite.next();
 			String key = (String) entry.getKey();
 			System.out.println("key:" + key);
 			Path path = (Path) entry.getValue();
@@ -71,16 +77,20 @@ public class Build {
 				Operation operation = opIte.next();
 				List<String> operationTags = operation.getTags();
 				System.out.println("opTag size:" + operationTags.size());
-				boolean flag = false;
-				for(String str:operationTags) {
-					if(list.contains(str)) {
-						flag = true;
-						break;
-					}
-				}
 				
-				if(!flag) {
-					opIte.remove();
+				//tags配置不为空时才进行过滤操作
+				if(!tags.equalsIgnoreCase("")) {
+					//过滤不需要的方法
+					boolean flag = false;
+					for(String str:operationTags) {
+						if(filterlist.contains(str)) {
+							flag = true;
+							break;
+						}
+					}
+					if(!flag) {
+						opIte.remove();
+					}
 				}
 			}
 			
@@ -94,7 +104,7 @@ public class Build {
 		        .withOutputLanguage(Language.ZH) 
 		        .withPathsGroupedBy(GroupBy.TAGS) 
 		        .build(); 
-		java.nio.file.Path outputFile = Paths.get("target/generated-docs/asciidoc/all");
+		java.nio.file.Path outputFile = Paths.get(outputFileLocation);
 		
 		Swagger2MarkupConverter.from(swagger) 
 		.withConfig(config)
