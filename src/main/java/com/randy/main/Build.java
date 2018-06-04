ad06209afc8abeaf663b338e76b5925f887bbddf
Randy <randy.xia98@gmail.com>
2018-06-04 19:06:51 +0800

    过滤不用显示的Model信息。

--- src/main/java/com/randy/main/Build.java
+++ src/main/java/com/randy/main/Build.java
@@ -33,129 +33,112 @@ import io.swagger.models.parameters.Parameter;
 import io.swagger.parser.SwaggerParser;
 
 public class Build {
-//	private final static String swaggerApi = "http://localhost:8081/iscs/v2/api-docs";
+	// private final static String swaggerApi =
+	// "http://localhost:8081/iscs/v2/api-docs";
 	private final static String swaggerApi = "http://localhost:8091/v2/api-docs";
 	private final static String outputFileLocation = "target/generated-docs/asciidoc/all";
 	private static Set<String> models = new HashSet<>();
-	
+
 	public static void main(String[] args) throws ConfigurationException, IOException {
 		Properties properties = new Properties();
-	    BufferedReader bufferedReader = new BufferedReader(new FileReader("config.properties"));
-	    properties.load(bufferedReader);
-	    String tags = properties.getProperty("tags");
-	    String[] array = tags.split(",");
-	    List<String> filterlist = Arrays.asList(array);
-	    System.out.println(tags+":"+filterlist.size());
-		
+		BufferedReader bufferedReader = new BufferedReader(new FileReader("config.properties"));
+		properties.load(bufferedReader);
+		String tags = properties.getProperty("tags");
+		String[] array = tags.split(",");
+		List<String> filterlist = Arrays.asList(array);
+		System.out.println(tags + ":" + filterlist.size());
+
 		Charset charset = Charset.defaultCharset();
 		System.out.println("charset:" + charset.name());
-		
+
 		Swagger swagger = new SwaggerParser().read(swaggerApi);
 		List<Tag> swTags = swagger.getTags();
 		Iterator<Tag> tagIte = swTags.iterator();
-		while(tagIte.hasNext()) {
+		while (tagIte.hasNext()) {
 			Tag tag = tagIte.next();
 			String name = tag.getName();
 			System.out.println("tag name:" + name);
-			
-			//tags配置不为空时才进行过滤操作
-			if(!tags.equalsIgnoreCase("")) {
-				if(name!=null && filterlist.contains(name)) {
+
+			// tags配置不为空时才进行过滤操作
+			if (!tags.equalsIgnoreCase("")) {
+				if (name != null && filterlist.contains(name)) {
 					System.out.println("remain tag:" + name);
-				}else {
-					//过滤不需要的tag
+				} else {
+					// 过滤不需要的tag
 					tagIte.remove();
 					System.out.println("remove tag:" + name);
-				}				
+				}
 			}
-
 		}
-		
-		Map<String,Path> paths = swagger.getPaths();
+
+		Map<String, Path> paths = swagger.getPaths();
 		Iterator<Map.Entry<String, Path>> ite = paths.entrySet().iterator();
-		while(ite.hasNext()) {
+		while (ite.hasNext()) {
 			Map.Entry<String, Path> entry = ite.next();
 			String key = (String) entry.getKey();
 			System.out.println("key:" + key);
 			Path path = (Path) entry.getValue();
 			List<Operation> operations = path.getOperations();
 			Iterator<Operation> opIte = operations.iterator();
-			while(opIte.hasNext()) {
+			while (opIte.hasNext()) {
 				Operation operation = opIte.next();
-				List<Scheme> schemes = operation.getSchemes();
-//				System.out.println("schemes:" + schemes);
-				if(schemes!=null) {
-					System.out.println("schemes size:" + schemes.size());
-					for(Scheme scheme:schemes) {
-						System.out.println("schemes class:" + scheme.getDeclaringClass());
-					}
-				}
-				
 				List<String> operationTags = operation.getTags();
 				System.out.println("opTag size:" + operationTags.size());
-				//tags配置不为空时才进行过滤操作
-				if(!tags.equalsIgnoreCase("")) {
-					//过滤不需要的方法
-					boolean flag = false;
-					for(String str:operationTags) {
-						if(filterlist.contains(str)) {
+
+				// 过滤不需要的方法
+				boolean flag = false;
+				// tags配置不为空时进行过滤操作
+				if (!tags.equalsIgnoreCase("")) {
+					for (String str : operationTags) {
+						if (filterlist.contains(str)) {
 							flag = true;
 							break;
 						}
 					}
-					if(!flag) {
-						opIte.remove();
-					}else {
-						List<Parameter> parameters = operation.getParameters();
-						System.out.println("parameters size:" + parameters.size());
-						for(Parameter parameter:parameters) {
-//							Map<String,Object> extensions = parameter.getVendorExtensions();
-//							String pattern = parameter.getPattern();
-//							System.out.println("parameter:Class:" + parameter.getClass());
-//							System.out.println("parameter:pattern:" + pattern);
-//							System.out.println("parameter:in:" + parameter.getIn());
-							
-							if(parameter instanceof BodyParameter) {
-								BodyParameter bodyParameter = (BodyParameter) parameter;
-								Model model = bodyParameter.getSchema();
-								if(model instanceof RefModel) {
-									RefModel refModel = (RefModel) model;
-									System.out.println("SimpleRef:" + refModel.getSimpleRef());
-									models.add(refModel.getSimpleRef());
-								}
-								System.out.println(model);
+				} else {
+					flag = true;
+				}
+
+				if (!flag) {
+					opIte.remove();
+				} else {
+					// 根据tag过滤方法参数中的Model对象。
+					List<Parameter> parameters = operation.getParameters();
+					System.out.println("parameters size:" + parameters.size());
+					for (Parameter parameter : parameters) {
+						if (parameter instanceof BodyParameter) {
+							BodyParameter bodyParameter = (BodyParameter) parameter;
+							Model model = bodyParameter.getSchema();
+							if (model instanceof RefModel) {
+								RefModel refModel = (RefModel) model;
+								System.out.println("SimpleRef:" + refModel.getSimpleRef());
+								models.add(refModel.getSimpleRef());
 							}
-						}						
+						}
 					}
 				}
 			}
-			
+
 			System.out.println("path:" + path);
 			System.out.println("------------------");
 		}
-		
-		Map<String,Model> definitions = swagger.getDefinitions();
+
+		Map<String, Model> definitions = swagger.getDefinitions();
 		Iterator<Map.Entry<String, Model>> defIte = definitions.entrySet().iterator();
-		while(defIte.hasNext()) {
+		while (defIte.hasNext()) {
 			Map.Entry<String, Model> definition = defIte.next();
 			String defName = definition.getKey();
-			if(models.contains(defName)) {
-			}else {
-				//过滤掉没用到的model
+			if (models.contains(defName)) {
+			} else {
+				// 过滤掉没用到的model。
 				defIte.remove();
 			}
 		}
-		
-		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder() 
-		        .withMarkupLanguage(MarkupLanguage.ASCIIDOC) 
-		        .withOutputLanguage(Language.ZH) 
-		        .withPathsGroupedBy(GroupBy.TAGS) 
-		        .build(); 
+
+		Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder().withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+				.withOutputLanguage(Language.ZH).withPathsGroupedBy(GroupBy.TAGS).build();
 		java.nio.file.Path outputFile = Paths.get(outputFileLocation);
-		
-		Swagger2MarkupConverter.from(swagger) 
-		.withConfig(config)
-        .build() 
-        .toFile(outputFile); 
+
+		Swagger2MarkupConverter.from(swagger).withConfig(config).build().toFile(outputFile);
 	}
 }
